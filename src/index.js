dba44bd4442bcba502afa279b613bdb98d127442
Diana De La Torre <didelatorre@utexas.edu>
2019-09-09 09:23:38 -0500

    Updated based on video tutorial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
+import Scene0 from './scenes/Scene0.js';
 import Config from './config/config.js';
+
 
 class Game extends Phaser.Game {
   constructor () {
     super(Config);
     this.scene.add('Boot', BootScene);
-    this.scene.start('Boot');
+    this.scene.add('Scene0', Scene0);
+    this.scene.start('Scene0');
   }
 }
 
